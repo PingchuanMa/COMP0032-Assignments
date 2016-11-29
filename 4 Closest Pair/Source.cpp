a3acc27ed3e8692056f79f558b89ab311c2fd4a3
Pika Ma <pika7ma@gmail.com>
2016-11-29 14:30:01 +0800

    Add Closest Pair.

--- 4 Closest Pair/Source.cpp
+++ 4 Closest Pair/Source.cpp
@@ -1,53 +1,66 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cmath>
+#include <map>
 using namespace std;
-typedef pair<unsigned int, unsigned int> point;
+typedef pair<int, int> point;
 typedef pair<point, point> points;
 bool less_x(const point &p1, const point &p2) {
     return p1.first < p2.first;
 }
-bool less_y(const point &p1, const point &p2) {
-    return p1.second < p2.second;
-}
-unsigned int sq_dist(const point& p1, const point& p2) {
+int sq_dist(const point& p1, const point& p2) {
     return (p1.first - p2.first) * (p1.first - p2.first) + (p1.second - p2.second) *(p1.second - p2.second);
 }
-unsigned int sq_dist(const points& p) {
+int sq_dist(const points& p) {
     return (p.first.first - p.second.first) * (p.first.first - p.second.first) + (p.first.second - p.second.second) *(p.first.second - p.second.second);
 }
-points closest_pair_rec(unsigned int px_l, unsigned int py_l, unsigned int range, const vector<point>& px, const vector<point>& py) {
-    // p_r = p_l + range
-    if (range == 1) {
-        return points(px[px_l], px[px_l + 1]);
+int closest_pair_rec(int left, int right, const vector<point>& px) {
+    if (left == right) {
+        return 2147483647;
     }
-    if (range == 2) {
-        points min(px[px_l], px[px_l + 1]);
-        if (sq_dist(min) > sq_dist(px[px_l + 1], px[px_l + 2])) {
-            min = points(px[px_l + 1], px[px_l + 2]);
+    if (left + 1 == right) {
+        return sq_dist(px[left], px[right]);
+    }
+    if (left + 2 == right) {
+        int min = sq_dist(px[left], px[left + 1]);
+        if (min > sq_dist(px[left + 1], px[right])) {
+            min = sq_dist(px[left + 1], px[right]);
         }
-        if (sq_dist(min) > sq_dist(px[px_l], px[px_l + 2])) {
-            return points(px[px_l], px[px_l + 2]);
+        if (min > sq_dist(px[left], px[right])) {
+            return sq_dist(px[left], px[right]);
         }
         return min;
     }
-    unsigned int middle_y = binary_search(py.begin() + py_l, py.begin() + py_l + range, px[px_l + range / 2].second);
-    points left = closest_pair_rec(px_l, );
-    
+    const int mid = (left + right) / 2;
+    int l_min = closest_pair_rec(left, mid, px);
+    int r_min = closest_pair_rec(mid + 1, right, px);
+    const int delta = (int)sqrt(min(l_min, r_min)) + 1;
+    int lr_min = min(l_min, r_min);
+    for (int i = mid; i >= left && px[mid].first - px[i].first <= delta; --i) {
+        for (int j = mid + 1; j <= right && px[j].first - px[mid].first <= delta; ++j) {
+            if (px[i].second - px[j].second <= delta && px[i].second - px[j].second >= -delta) {
+                int temp = sq_dist(px[i], px[j]);
+                if (temp < lr_min) {
+                    lr_min = temp;
+                }
+            }
+        }
+    }
+    return lr_min;
 }
 int main()
 {
-    unsigned int num_of_point = 0;
-    unsigned int x, y;
+    int num_of_point = 0;
+    int x, y;
     cin >> num_of_point;
     vector<point> px;
     for (int i = 0; i < num_of_point; ++i) {
         cin >> x >> y;
         px.push_back(point(x, y));
     }
-    vector<point> py(px);
     sort(px.begin(), px.end(), less_x);
-    sort(py.begin(), py.end(), less_y);
-    points min = closest_pair_rec(0, 0, px.size() - 1, px, py);
+    int min = closest_pair_rec(0, px.size() - 1, px);
+    cout << min;
     return 0;
 }
