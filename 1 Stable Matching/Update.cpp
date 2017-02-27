717d3b0a8ec8e2cd4e580f9a90aab53b0fc2b3ba
Pika Ma <pika7ma@gmail.com>
2017-02-27 14:14:55 +0800

    Update problem 1.

--- 1 Stable Matching/Update.cpp
+++ 1 Stable Matching/Update.cpp
@@ -4,26 +4,49 @@
 using namespace std;
 int main() {
     int preset;
-    int temp;
+    int man, woman;
     cin >> preset;
     vector<queue<int> > rankM(preset, queue<int>());
     vector<vector<int> > rankW(preset, vector<int>(preset));
-    vector<int> ans(preset, -1);
+    vector<int> ansM(preset, -1);
+    vector<int> ansW(preset, -1);
     queue<int> men;
-    for (int i = 0; i < preset; men.push(i++));
     for (int i = 0; i < preset; ++i) {
+        men.push(i);
         for (int j = 0; j < preset; ++j) {
-            cin >> temp;
-            rankM[i].push(temp);
+            cin >> woman;
+            rankM[i].push(woman - 1);
         }
     }
     for (int i = 0; i < preset; ++i) {
-        for (int j = 0; i < preset; ++j) {
-            cin >> temp;
-            rankW[i][temp - 1] = j;
+        for (int j = 0; j < preset; ++j) {
+            cin >> man;
+            rankW[i][man - 1] = j;
         }
     }
     while (men.empty() != true) {
-
+        man = men.front();
+        men.pop();
+        while (true) {
+            woman = rankM[man].front();
+            rankM[man].pop();
+            if (ansW[woman] == -1) {
+                ansW[woman] = man;
+                ansM[man] = woman;
+                break;
+            }
+            else if (rankW[woman][man] < rankW[woman][ansW[woman]]) {
+                ansM[ansW[woman]] = -1;
+                men.push(ansW[woman]);
+                ansW[woman] = man;
+                ansM[man] = woman;
+                break;
+            }
+        }
     }
+    for (int i = 0; i < preset; ++i) {
+        cout << ansM[i] + 1 << " ";
+    }
+    cout << endl;
+    return 0;
 }
