c2c146e5d7c53984bc0f1bab2e01e55e722e4118
Pika Ma <pika7ma@gmail.com>
2017-05-22 22:54:53 +0800

    Update.

--- 0 Tic-tac-toe (Final Project)/Source.cpp
+++ 0 Tic-tac-toe (Final Project)/Source.cpp
@@ -1,287 +1,284 @@
 #include <iostream>
 using namespace std;
 
-#define INF 999
-#define NINF -999
+class Tic_tac_toe {
 
-struct state {
-    int xo[3][3];
-    int x_i, o_i;
+private:
+    int board[3][3];
+    int x_value, o_value;
     int value;
-    int human;
-    int x_w, o_w;
+    int turn;
+
+public:
+    static const int MAX_VALUE = 999;
+    static const int MIN_VALUE = -999;
+    static const int X = 1;
+    static const int O = -1;
+
+    Tic_tac_toe() {
+        memset(board, 0, sizeof(board));
+        x_value = 0;
+        o_value = 0;
+        value = 0;
+        choose_order();
+        start_game();
+    }
+
+    void compute_value() {
+        x_value = 0;
+        o_value = 0;
+        if ((board[0][0] != O) && (board[0][1] != O) && (board[0][2] != O))
+            x_value++;
+        if ((board[1][0] != O) && (board[1][1] != O) && (board[1][2] != O))
+            x_value++;
+        if ((board[2][0] != O) && (board[2][1] != O) && (board[2][2] != O))
+            x_value++;
+        if ((board[0][0] != O) && (board[1][0] != O) && (board[2][0] != O))
+            x_value++;
+        if ((board[0][1] != O) && (board[1][1] != O) && (board[2][1] != O))
+            x_value++;
+        if ((board[0][2] != O) && (board[1][2] != O) && (board[2][2] != O))
+            x_value++;
+        if ((board[0][0] != O) && (board[1][1] != O) && (board[2][2] != O))
+            x_value++;
+        if ((board[0][2] != O) && (board[1][1] != O) && (board[2][0] != O))
+            x_value++;
+
+        if ((board[0][0] != X) && (board[0][1] != X) && (board[0][2] != X))
+            o_value++;
+        if ((board[1][0] != X) && (board[1][1] != X) && (board[1][2] != X))
+            o_value++;
+        if ((board[2][0] != X) && (board[2][1] != X) && (board[2][2] != X))
+            o_value++;
+        if ((board[0][0] != X) && (board[1][0] != X) && (board[2][0] != X))
+            o_value++;
+        if ((board[0][1] != X) && (board[1][1] != X) && (board[2][1] != X))
+            o_value++;
+        if ((board[0][2] != X) && (board[1][2] != X) && (board[2][2] != X))
+            o_value++;
+        if ((board[0][0] != X) && (board[1][1] != X) && (board[2][2] != X))
+            o_value++;
+        if ((board[0][2] != X) && (board[1][1] != X) && (board[2][0] != X))
+            o_value++;
+
+        value = x_value - o_value;
+
+        if ((board[0][0] + board[0][1] + board[0][2] == 3) ||
+            (board[1][0] + board[1][1] + board[1][2] == 3) ||
+            (board[2][0] + board[2][1] + board[2][2] == 3) ||
+            (board[0][0] + board[1][0] + board[2][0] == 3) ||
+            (board[0][1] + board[1][1] + board[2][1] == 3) ||
+            (board[0][2] + board[1][2] + board[2][2] == 3) ||
+            (board[0][0] + board[1][1] + board[2][2] == 3) ||
+            (board[0][2] + board[1][1] + board[2][0] == 3)) {
+            value = MAX_VALUE;
+        }
+
+        if ((board[0][0] + board[0][1] + board[0][2] == -3) ||
+            (board[1][0] + board[1][1] + board[1][2] == -3) ||
+            (board[2][0] + board[2][1] + board[2][2] == -3) ||
+            (board[0][0] + board[1][0] + board[2][0] == -3) ||
+            (board[0][1] + board[1][1] + board[2][1] == -3) ||
+            (board[0][2] + board[1][2] + board[2][2] == -3) ||
+            (board[0][0] + board[1][1] + board[2][2] == -3) ||
+            (board[0][2] + board[1][1] + board[2][0] == -3)) {
+            value = MIN_VALUE;
+        }
+    }
+
+    void input() {
+        int x, y;
+        cin >> x >> y;
+        if (x < 1 || x > 3 || y < 1 || y > 3) {
+            cout << "咱井字棋棋盘小，您朝里边儿下。" << endl;
+            input();
+        }
+        else if (board[x - 1][y - 1] == 1 || board[x - 1][y - 1] == -1) {
+            cout << "这儿有子儿了，您别处请吧。" << endl;
+            input();
+        }
+        else { board[x - 1][y - 1] = turn; }
+    }
+
+    void choose_order() {
+        int turn_;
+        cout << "按1: 您先请。" << endl;
+        cout << "按2: 我就不客气了。" << endl;
+        cin >> turn_;
+        if (turn_ == 1) { turn = 1; }
+        else if (turn_ == 2) { turn = -1; }
+        else {
+            cout << "您这是让谁下呢？" << endl;
+            choose_order();
+        }
+    }
+
+
+    int max2() {
+        int i, j;
+        int max = MIN_VALUE;
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                if (board[i][j] == 0) {//该位置可下
+                    board[i][j] = turn;
+                    compute_value();
+                    if (value == MIN_VALUE) {
+                        board[i][j] = 0;
+                        return MIN_VALUE;
+                    }
+                    if (value > max)
+                        max = value;
+                    board[i][j] = 0;
+                }
+            }
+        }
+        return max;
+    }
+
+    int min1() {
+        int i, j;
+        int min = MAX_VALUE;
+        int best;
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                if (board[i][j] == 0) {//该位置可下
+                    board[i][j] = -turn;
+                    compute_value();
+                    value = max2();
+                    if (min >= value) {
+                        min = value;
+                        best = i * 10 + j;
+                    }
+                    board[i][j] = 0;
+                }
+            }
+        }
+        board[best / 10][best % 10] = -turn;
+        return 0;
+    }
+
+    int min2() {
+        int i, j;
+        int min = MAX_VALUE;
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                if (board[i][j] == 0) {//该位置可下
+                    board[i][j] = turn;
+                    compute_value();
+                    if (value == MAX_VALUE) {
+                        board[i][j] = 0;
+                        return MAX_VALUE;
+                    }
+                    if (value < min)
+                        min = value;
+                    board[i][j] = 0;
+                }
+            }
+        }
+        return min;
+    }
+
+    int max1() {
+        int max = MIN_VALUE;
+        int best = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 0) {//该位置可下
+                    board[i][j] = -turn;
+                    compute_value();
+                    value = min2();
+                    if (max <= value) {
+                        max = value;
+                        best = i * 10 + j;
+                    }
+                    board[i][j] = 0;
+                }
+            }
+        }
+        board[best / 10][best % 10] = -turn;
+        return 0;
+    }
+
+    void print() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 1)
+                    cout << "X ";
+                else if (board[i][j] == -1)
+                    cout << "O ";
+                else
+                    cout << "* ";
+            }
+            cout << endl;
+        }
+    }
+
+    int start_game() {
+        printf("您棋盘长这样：\n");
+        compute_value();
+        print();
+        if (turn == 1) {
+            while (x_value != 0 && o_value != 0) {
+                cout << endl << "轮您下子儿：" << endl;
+                input();
+                compute_value();
+                print();
+                if (value == MAX_VALUE) {
+                    cout << "哇心态爆炸！" << endl;
+                    return 0;
+                }
+                if (x_value == 0 && o_value == 0) {
+                    cout << "平局嘿！" << endl;
+                    return 0;
+                }
+                cout << endl << "我的嘞：" << endl;
+                min1();
+                compute_value();
+                print();
+                if (value == MIN_VALUE) {
+                    cout << endl << "输了吧略略略！" << endl;
+                    return 0;
+                }
+                if (x_value == 0 && o_value == 0) {
+                    cout << "平局嘿！" << endl;
+                    return 0;
+                }
+            }
+        }
+        else if (turn == -1) {
+            while (x_value != 0 && o_value != 0) {
+                cout << endl << "我的嘞：" << endl;
+                max1();
+                compute_value();
+                print();
+                if (value == MAX_VALUE) {
+                    cout << endl << "输了吧略略略！" << endl;
+                    return 0;
+                }
+                if (x_value == 0 && o_value == 0) {
+                    cout << "平局嘿！" << endl;
+                    return 0;
+                }
+                cout << endl << "轮您下子儿：" << endl;
+                compute_value();
+                print();
+                if (value == MIN_VALUE) {
+                    cout << "哇心态爆炸！" << endl;
+                    return 0;
+                }
+                if (x_value == 0 && o_value == 0) {
+                    cout << "平局嘿！" << endl;
+                    return 0;
+                }
+            }
+        }
+        return 0;
+    }
 };
 
-void ini(state &s) {
-    int i, j;
-    for (i = 0; i<3; i++)
-        for (j = 0; j<3; j++)
-            s.xo[i][j] = 0;
-}
-
-void sup(state &s) {
-    s.x_i = 0;
-    s.o_i = 0;
-    if ((s.xo[0][0] == 0 || s.xo[0][0] == 1) && (s.xo[0][1] == 0 || s.xo[0][1] == 1) && (s.xo[0][2] == 0 || s.xo[0][2] == 1))
-        s.x_i++;
-    if ((s.xo[1][0] == 0 || s.xo[1][0] == 1) && (s.xo[1][1] == 0 || s.xo[1][1] == 1) && (s.xo[1][2] == 0 || s.xo[1][2] == 1))
-        s.x_i++;
-    if ((s.xo[2][0] == 0 || s.xo[2][0] == 1) && (s.xo[2][1] == 0 || s.xo[2][1] == 1) && (s.xo[2][2] == 0 || s.xo[2][2] == 1))
-        s.x_i++;
-    if ((s.xo[0][0] == 0 || s.xo[0][0] == 1) && (s.xo[1][0] == 0 || s.xo[1][0] == 1) && (s.xo[2][0] == 0 || s.xo[2][0] == 1))
-        s.x_i++;
-    if ((s.xo[0][1] == 0 || s.xo[0][1] == 1) && (s.xo[1][1] == 0 || s.xo[1][1] == 1) && (s.xo[2][1] == 0 || s.xo[2][1] == 1))
-        s.x_i++;
-    if ((s.xo[0][2] == 0 || s.xo[0][2] == 1) && (s.xo[1][2] == 0 || s.xo[1][2] == 1) && (s.xo[2][2] == 0 || s.xo[2][2] == 1))
-        s.x_i++;
-    if ((s.xo[0][0] == 0 || s.xo[0][0] == 1) && (s.xo[1][1] == 0 || s.xo[1][1] == 1) && (s.xo[2][2] == 0 || s.xo[2][2] == 1))
-        s.x_i++;
-    if ((s.xo[0][2] == 0 || s.xo[0][2] == 1) && (s.xo[1][1] == 0 || s.xo[1][1] == 1) && (s.xo[2][0] == 0 || s.xo[2][0] == 1))
-        s.x_i++;
-
-    if ((s.xo[0][0] == 0 || s.xo[0][0] == -1) && (s.xo[0][1] == 0 || s.xo[0][1] == -1) && (s.xo[0][2] == 0 || s.xo[0][2] == -1))
-        s.o_i++;
-    if ((s.xo[1][0] == 0 || s.xo[1][0] == -1) && (s.xo[1][1] == 0 || s.xo[1][1] == -1) && (s.xo[1][2] == 0 || s.xo[1][2] == -1))
-        s.o_i++;
-    if ((s.xo[2][0] == 0 || s.xo[2][0] == -1) && (s.xo[2][1] == 0 || s.xo[2][1] == -1) && (s.xo[2][2] == 0 || s.xo[2][2] == -1))
-        s.o_i++;
-    if ((s.xo[0][0] == 0 || s.xo[0][0] == -1) && (s.xo[1][0] == 0 || s.xo[1][0] == -1) && (s.xo[2][0] == 0 || s.xo[2][0] == -1))
-        s.o_i++;
-    if ((s.xo[0][1] == 0 || s.xo[0][1] == -1) && (s.xo[1][1] == 0 || s.xo[1][1] == -1) && (s.xo[2][1] == 0 || s.xo[2][1] == -1))
-        s.o_i++;
-    if ((s.xo[0][2] == 0 || s.xo[0][2] == -1) && (s.xo[1][2] == 0 || s.xo[1][2] == -1) && (s.xo[2][2] == 0 || s.xo[2][2] == -1))
-        s.o_i++;
-    if ((s.xo[0][0] == 0 || s.xo[0][0] == -1) && (s.xo[1][1] == 0 || s.xo[1][1] == -1) && (s.xo[2][2] == 0 || s.xo[2][2] == -1))
-        s.o_i++;
-    if ((s.xo[0][2] == 0 || s.xo[0][2] == -1) && (s.xo[1][1] == 0 || s.xo[1][1] == -1) && (s.xo[2][0] == 0 || s.xo[2][0] == -1))
-        s.o_i++;
-
-    s.value = s.x_i - s.o_i;
-
-    if ((s.xo[0][0] + s.xo[0][1] + s.xo[0][2] == 3) || (s.xo[1][0] + s.xo[1][1] + s.xo[1][2] == 3) || (s.xo[2][0] + s.xo[2][1] + s.xo[2][2] == 3) ||
-        (s.xo[0][0] + s.xo[1][0] + s.xo[2][0] == 3) || (s.xo[0][1] + s.xo[1][1] + s.xo[2][1] == 3) || (s.xo[0][2] + s.xo[1][2] + s.xo[2][2] == 3) ||
-        (s.xo[0][0] + s.xo[1][1] + s.xo[2][2] == 3) || (s.xo[0][2] + s.xo[1][1] + s.xo[2][0] == 3))
-        s.value = INF;
-    if ((s.xo[0][0] + s.xo[0][1] + s.xo[0][2] == -3) || (s.xo[1][0] + s.xo[1][1] + s.xo[1][2] == -3) || (s.xo[2][0] + s.xo[2][1] + s.xo[2][2] == -3) ||
-        (s.xo[0][0] + s.xo[1][0] + s.xo[2][0] == -3) || (s.xo[0][1] + s.xo[1][1] + s.xo[2][1] == -3) || (s.xo[0][2] + s.xo[1][2] + s.xo[2][2] == -3) ||
-        (s.xo[0][0] + s.xo[1][1] + s.xo[2][2] == -3) || (s.xo[0][2] + s.xo[1][1] + s.xo[2][0] == -3))
-        s.value = NINF;
-}
-
-int waitfor(state &s) {
-    int x, y;
-    x = 0;
-    y = 0;
-    cin >> x >> y;
-    x = x - 1;
-    y = y - 1;
-    if (x<0 || x>2 || y<0 || y>2) {
-        printf("我们的棋盘是3x3的，您不能把棋子下到棋盘外面，请重新落子\n");
-        printf("x=%d  y=%d (x,y)=%d\n", x, y, s.xo[x][y]);
-        waitfor(s);
-    }
-    else if (s.xo[x][y] == 1 || s.xo[x][y] == -1) {
-        printf("此处已有棋子，请重新落子\n");
-        printf("x=%d  y=%d (x,y)=%d\n", x, y, s.xo[x][y]);
-        waitfor(s);
-    }
-    else
-        s.xo[x][y] = s.human;
-    return 0;
-}
-
-int chooseorder(state &s) {
-    int c;
-    printf("选择由谁先下：\n1.人先落子\n2.电脑先落子\n");
-    cin >> c;
-    if (c == 1)	s.human = 1;
-    else if (c == 2)	s.human = -1;
-    else {
-        printf("输入了不正确的值，请重新输入");
-        chooseorder(s);
-    }
-    return 0;
-}
-
-int max1(state &s) {
-    int i, j;
-    int max = NINF;
-    for (i = 0; i<3; i++) {
-        for (j = 0; j<3; j++) {
-            if (s.xo[i][j] == 0) {//该位置可下
-                s.xo[i][j] = s.human;
-                sup(s);
-                if (s.value == NINF) {
-                    s.xo[i][j] = 0;
-                    return NINF;
-                }
-                if (s.value>max)
-                    max = s.value;
-                s.xo[i][j] = 0;
-            }
-            else
-                continue;
-        }
-    }
-    return max;
-}
-
-int min1(state &s) {
-    int i, j;
-    int min = INF;
-    int best;
-    for (i = 0; i<3; i++) {
-        for (j = 0; j<3; j++) {
-            if (s.xo[i][j] == 0) {//该位置可下
-                s.xo[i][j] = -s.human;
-                sup(s);
-                s.value = max1(s);
-                if (min >= s.value) {
-                    min = s.value;
-                    best = i * 10 + j;
-                }
-                s.xo[i][j] = 0;
-            }
-            else
-                continue;
-        }
-    }
-    s.xo[best / 10][best % 10] = -s.human;
-    return 0;
-}
-
-int min2(state &s) {
-    int i, j;
-    int min = INF;
-    for (i = 0; i<3; i++) {
-        for (j = 0; j<3; j++) {
-            if (s.xo[i][j] == 0) {//该位置可下
-                s.xo[i][j] = s.human;
-                sup(s);
-                if (s.value == INF) {
-                    s.xo[i][j] = 0;
-                    return INF;
-                }
-                if (s.value<min)
-                    min = s.value;
-                s.xo[i][j] = 0;
-            }
-            else
-                continue;
-        }
-    }
-    return min;
-}
-
-int max2(state &s) {
-    int i, j;
-    int max = NINF;
-    int best;
-    for (i = 0; i<3; i++) {
-        for (j = 0; j<3; j++) {
-            if (s.xo[i][j] == 0) {//该位置可下
-                s.xo[i][j] = -s.human;
-                sup(s);
-                s.value = min2(s);
-                if (max <= s.value) {
-                    max = s.value;
-                    best = i * 10 + j;
-                }
-                s.xo[i][j] = 0;
-            }
-            else
-                continue;
-        }
-    }
-    s.xo[best / 10][best % 10] = -s.human;
-    return 0;
-}
-
-void printstate(state s) {
-    int i, j;
-    for (i = 0; i<3; i++) {
-        for (j = 0; j<3; j++) {
-            if (s.xo[i][j] == 1)
-                printf("x  ");
-            else if (s.xo[i][j] == -1)
-                printf("o  ");
-            else
-                printf("*  ");
-        }
-        printf("\n");
-    }
-}
-
-int tic_tac_toe(state &s) {
-    printf("initial state\n");
-    ini(s);
-    sup(s);
-    printstate(s);
-    if (s.human == 1) {
-        while (s.x_i != 0 && s.o_i != 0) {
-            printf("\nyour turn\n");
-            waitfor(s);
-            sup(s);
-            printstate(s);
-            if (s.value == INF) {
-                printf("EXECELLENT!\nYOU WIN\n");
-                return 0;
-            }
-            if (s.x_i == 0 && s.o_i == 0) {
-                printf("DRAW\n");
-                return 0;
-            }
-            printf("\ncomputer turn\n");
-            min1(s);
-            sup(s);
-            printstate(s);
-            if (s.value == NINF) {
-                printf("YOU LOSE\n");
-                return 0;
-            }
-            if (s.x_i == 0 && s.o_i == 0) {
-                printf("DRAW\n");
-                return 0;
-            }
-        }
-    }
-    else if (s.human == -1) {
-        while (s.x_i != 0 && s.o_i != 0) {
-            printf("\ncomputer turn\n");
-            max2(s);
-            sup(s);
-            printstate(s);
-            if (s.value == INF) {
-                printf("YOU LOSE\n");
-                return 0;
-            }
-            if (s.x_i == 0 && s.o_i == 0) {
-                printf("DRAW\n");
-                return 0;
-            }
-            printf("\nyour turn\n");
-            if (waitfor(s) == -1)
-                return -1;
-            sup(s);
-            printstate(s);
-            if (s.value == NINF) {
-                printf("EXECELLENT!\nYOU WIN\n");
-                return 0;
-            }
-            if (s.x_i == 0 && s.o_i == 0) {
-                printf("DRAW\n");
-                return 0;
-            }
-        }
-    }
-    return 0;
-}
 
 int main()
 {
-    state s;
-    chooseorder(s);
-    if (tic_tac_toe(s) == -1)
-        return -1;
+    Tic_tac_toe game;
     return 0;
 }
